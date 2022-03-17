11164d422997c40ae45f880480ec95e82972940e
Alexey <miasalexln@gmail.com>
2022-03-17 11:47:59 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Completed Tasks
+
+### Get level 5 on CodeWars [Screen](/CodeWarsScreen/level5CodeWars.jpg)
+
+### Task04 Stalin sort [Solution](/Task01_04_StalinSort/)
---
+++ Task01_04_StalinSort/Task01_04_StalinSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Int32;
+
+// input from console numbers
+Console.WriteLine("Please enter numbers over space");
+var inputText = Console.ReadLine();
+var splitInputText = inputText?.Split(' ');
+var numberList = new List<int>();
+if (splitInputText != null)
+    foreach (var number in splitInputText)
+    {
+        if (TryParse(number, out var j))
+        {
+            numberList.Add(j);
+        }
+    }
+
+// Call function 
+var listAfterStalinSort = Task4(numberList);
+
+// outPut result 
+foreach (var number in listAfterStalinSort)
+{
+    Console.Write($"{number} ");
+}
+
+// Task4 function 
+static List<int> Task4(IReadOnlyList<int> array)
+{
+    // initialize a temporary leaf with the maximum possible capacity
+    var res = new List<int>(array.Count);
+    for (var i = array.Count - 1; i > 0; i--)
+    {
+        if (array[i] >= array[i - 1])
+        {
+            res.Add(array[i]);
+        }
+    }
+
+    // the first leftmost number is added anyway
+    res.Add(array[0]);
+    res.Reverse();
+    return res;
+}
